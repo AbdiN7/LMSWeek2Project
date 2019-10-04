e87e9a46f0bf46ff550d14005cbf529e4b9dc67a
Abdi <abdirahman.noor@smoothstack.com>
2019-10-03 21:19:42 -0400

    added Author / book / publisher Entities

---
+++ src/com/ss/lms/Model/Publisher.java
@@ -0,0 +1,31 @@
+package com.ss.lms.Model;
+
+public class Publisher {
+    private Integer publisherId;
+    private String publisherName;
+    private String publisherAddress;
+
+    public Integer getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(Integer publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getPublisherAddress() {
+        return publisherAddress;
+    }
+
+    public void setPublisherAddress(String publisherAddress) {
+        this.publisherAddress = publisherAddress;
+    }
+}
