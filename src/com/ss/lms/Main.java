9c9bc83e5ddb091e970144b5e1b99efe26aa4534
Abdi <abdirahman.noor@smoothstack.com>
2019-10-04 20:38:50 -0400

    Uncommenting code

--- src/com/ss/lms/Main.java
+++ src/com/ss/lms/Main.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 
 public class Main {
 
-//
-//	static UI ui = new UI();
-//	static BorrowerView borrowerView = new BorrowerView();
+
+	static UI ui = new UI();
+	static BorrowerView borrowerView = new BorrowerView();
 
 	public static Scanner userInput = new Scanner(System.in);
 	
@@ -21,35 +21,35 @@ public class Main {
 		AdminServices myAdmin = new AdminServices();
 
 
-//		char input = ' ';
-//
-//		ui.mainMenuTop();
-//
-//		while (true) {
-//
-//			ui.mainMenu();
-//
-//			input = userInput.next().charAt(0);
-//
-//			ui.menuBoxBottom();
-//
-//			if (input == '1') {
-//
-//			} else if (input == '2') {
-//
-//			} else if (input == '3') {
-//				borrowerView.menuBorrower();
-//
-//			} else if (input == '0') {
-//				break;
-//			} else {
-//				System.out.println("Not a choice!");
-//			}
-//
-//			ui.mainMenuBottom();
-//
-//		}
-//		ui.mainMenuQuit();
-//		userInput.close();
+		char input = ' ';
+
+		ui.mainMenuTop();
+
+		while (true) {
+
+			ui.mainMenu();
+
+			input = userInput.next().charAt(0);
+
+			ui.menuBoxBottom();
+
+			if (input == '1') {
+
+			} else if (input == '2') {
+
+			} else if (input == '3') {
+				borrowerView.menuBorrower();
+
+			} else if (input == '0') {
+				break;
+			} else {
+				System.out.println("Not a choice!");
+			}
+
+			ui.mainMenuBottom();
+
+		}
+		ui.mainMenuQuit();
+		userInput.close();
 	}
 }
