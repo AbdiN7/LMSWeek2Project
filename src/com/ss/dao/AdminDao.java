5338f030fc2d8327a8f733c393e13709669ae1e1
Alfred <alfredo.martinez.smoothstack.com>
2019-10-06 13:14:26 -0400

    commit test

---
+++ src/com/ss/dao/AdminDao.java
@@ -0,0 +1,34 @@
+package com.ss.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AdminDao {
+
+    public  void deleteBorrower(int cardNo) throws SQLException
+    {
+		/*
+		 * Connection connection = DriverManager.getConnection(myUrl.getUrl());
+		 * PreparedStatement st =
+		 * connection.prepareStatement("DELETE FROM tbl_borrower WHERE cardNo = ? ");
+		 * st.setString(1, String.valueOf(cardNo)); st.executeUpdate();
+		 * connection.close();
+		 */
+    }
+    public  void addBorrower(int cardNo, String name, String address, String phone) throws SQLException
+    {
+
+		/*
+		 * Connection connection = DriverManager.getConnection(myUrl.getUrl());
+		 * PreparedStatement st = connection.
+		 * prepareStatement("insert into tbl_borrower (cardNo, name, address, phone)" +
+		 * "VALUES (?,?,?,?)"); st.setString(1, String.valueOf(cardNo));
+		 * st.setString(2,name); st.setString(3,address); st.setString(4,phone);
+		 * st.executeUpdate(); connection.close();
+		 */
+    }
+    
+
+}
